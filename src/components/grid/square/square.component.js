32ddfc9836a904d3ed73f20292c62fd0bd1feeab
Pier Yos <khmaipapi@yahoo.com>
2019-11-25 16:57:24 -0500

    fix hovering not showing default color bug

--- src/components/grid/square/square.component.js
+++ src/components/grid/square/square.component.js
@@ -5,12 +5,13 @@ export default class Square extends PureComponent {
  
     render() {
         const {row,col,mouseIsOver,mouseIsDown,color} = this.props;
-        const moreClass = mouseIsDown ? 'isSelected':
-                        mouseIsOver ? 'isHover':'';
+        const moreClass = mouseIsOver ? 'isHover':'';
+        const style = color === 'rgb(241,241,241)' ? {}:
+                        mouseIsDown ? {backgroundColor: color}:null;
         return (
             <div 
-                className={`${classes.square} ${moreClass}`}
-                style={mouseIsDown ? {backgroundColor: color}:null}
+                className={`${classes.square} ${moreClass ? classes[moreClass]:''}`}
+                style={style}
                 id={`r${row}c${col}`}
                 onMouseDown={() => this.props.handleMouseDown(row,col)}
                 onMouseUp={() => this.props.handleMouseUp()}
